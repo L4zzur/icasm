6c5f64542651997b21671f01a336b2c6d516bc97
Popov Nikita <popovnikita01@gmail.com>
2024-09-28 20:25:17 +0200

    Описал модель пользователя

--- backend/app/core/models/__init__.py
+++ backend/app/core/models/__init__.py
@@ -1,3 +1,8 @@
-__all__ = ["db_helper"]
+__all__ = [
+    "db_helper",
+    "User",
+]
 
+from .base import Base
 from .db_helper import db_helper
+from .user import User
---
+++ backend/app/core/models/user.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+from .mixins import IdMixin
+
+
+class User(Base, IdMixin):
+    tg_id: Mapped[int] = mapped_column(
+        nullable=False,
+        unique=True,
+        comment="Telegram ID пользователя",
+    )
+    name: Mapped[str] = mapped_column(
+        nullable=False,
+        comment="Имя пользователя",
+    )
+    username: Mapped[str] = mapped_column(
+        nullable=True,
+        comment="Юзернейм пользователя",
+    )
+    is_premium: Mapped[bool] = mapped_column(
+        nullable=False,
+        default=False,
+        comment="Куплен ли премиум",
+    )
