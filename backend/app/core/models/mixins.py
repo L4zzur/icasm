19dcc39651ed27d547666624913e563ebc4c4cf8
Popov Nikita <popovnikita01@gmail.com>
2024-10-02 14:01:19 +0200

    Описал миксин для даты создания

--- backend/app/core/models/mixins.py
+++ backend/app/core/models/mixins.py
@@ -1,5 +1,7 @@
+from datetime import datetime, timezone
 from uuid import UUID, uuid4
 
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
 
 
@@ -10,4 +12,12 @@ class IdMixin:
         nullable=False,
         default=uuid4,
         comment="Идентификатор",
+    )
+
+
+class CreatedAtMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.now(timezone.utc),
+        server_default=func.now(),
+        comment="Время создания",
     )
